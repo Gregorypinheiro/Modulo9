2b47db4efa7bb8d8682a567159c1193ffa33c956
gregory <gregorypinheiro88@gmail.com>
2024-05-08 18:19:57 -0300

    fim do modulo 09
    jQuery

--- main.js
+++ main.js
@@ -8,8 +8,20 @@ $(document).ready(function(){
     })
 
     $('form').on('submit', function(e) {
-        console.log("submit");
         e.preventDefault();
+        const endecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src="${endecoDaNovaImagem}" />`).appendTo(novoItem);
+        $(`
+            <div class="overlay-imagem-link">
+                <a href="${endecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+            `).appendTo(novoItem);
+            $(novoItem).appendTo('ul');
+            $(novoItem).fadeIn(1000);
+            $('#endereco-imagem-nova').val('');
     }) 
 })
 
